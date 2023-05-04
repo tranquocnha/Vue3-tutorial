b89fb69fee9e0d455c55a259bb734412ece4b3a2
tranquocnha <tranquocnha180800@gmail.com>
2023-05-04 10:37:45 +0700

    update

---
+++ vue3-tuorial-mini-pokemon-ytb/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <main-screen />
+  <coppy-right-screen />
+  <interact-screen />
+</template>
+
+<script>
+import Main from "./components/MainScreen.vue";
+import CoppyRightScreen from "./components/CoppyRightScreen.vue";
+import InteractScreen from "./components/InteractScreen.vue";
+
+export default {
+  name: "App",
+  components: {
+    MainScreen: Main,
+    CoppyRightScreen,
+    InteractScreen,
+  },
+};
+</script>
